a138fc0582b72587a6df0b6f8c261c83401802b0
Nishit Padiya <90115922+IAM-NEYE@users.noreply.github.com>
2022-11-30 14:32:51 +0000

    Added symbols

--- Main.java
+++ Main.java
@@ -8,14 +8,15 @@ class Main
       int discount=10;
       double amount=purchase-(purchase*discount)/100;
         //Display
-      if(purchase>=5000)
+      
+    if(purchase>=5000)
       {
-           System.out.println("purchase :"+purchase);
-           System.out.println("discount :"+discount);
-           System.out.println("Total amount :"+amount);    
+           System.out.println("purchase :"+purchase+"Rs");
+           System.out.println("discount :"+discount+"%");
+           System.out.println("Total amount :"+amount+"Rs");    
       }//end of if
     else
-        {System.out.println("total amount :"+purchase);
+        {System.out.println("total amount :"+purchase+"Rs");
           }//end of else
   }//end of main
 }//end of class
